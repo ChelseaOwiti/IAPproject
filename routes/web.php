f94ea670d7e6de828fa79e921fb2f3f975237925
Chelsea Owiti <chelseaowiti@gmail.com>
2021-04-12 02:49:28 +0300

    done but problems to resolve

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/register', 'RegistrationController@create');
-Route::post('register', 'RegistrationController@store');
 
-Route::get('/login', 'SessionsController@create');
-Route::post('/login', 'SessionsController@store');
-Route::get('/logout', 'SessionsController@destroy');
+
+Route::post('/auth/save',[MainController::class, 'save'])->name('auth.save');
+Route::post('/auth/check',[MainController::class, 'check'])->name('auth.check');
+
+
+Route::get('/auth/logout',[MainController::class, 'logout'])->name('auth.logout');
+
+Route::group(['middleware'=>['AuthCheck']], function(){
+    Route::get('/auth/login',[MainController::class, 'login'])->name('auth.login');
+    Route::get('/auth/register',[MainController::class, 'register'])->name('auth.register');
+    Route::get('/admin/dashboard',[MainController::class, 'dashboard']);
+});
